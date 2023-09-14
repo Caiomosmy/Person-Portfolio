dfec09b6b096f78796ac1dfc399853731edcc6f5
Caio Henrique - Full Stack Python <Caiomosmy@outlook.com>
2023-09-14 18:59:37 -0300

    Update nodeServer.js

--- nodeServer.js
+++ nodeServer.js
@@ -4,8 +4,9 @@ const express = require ('express');
 const app = express();
 const port = 3000
 
-/*Colocar o caminho do arquivo*/
+/*Colocar o caminho do arquivo aonde espera ter o HTML*/
+/*Veja se tem o HTML no arquivo template dentro da pasta node*/ 
 
 const path = require("path")
-
+const basePath = path.join(__dirname, "index.html")
 /**/
